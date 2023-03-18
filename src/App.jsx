4688f06f65b671fdc4d27db30878b6915b55f452
Kavindu Theekshana <kavindutheekshana@gmail.com>
2023-03-18 01:14:58 +0000

    sdds

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// import { BrowserRouter } from "react-router-dom";
+// import {
+//   About,
+//   Contact,
+//   Experience,
+//   Feedbacks,
+//   Hero,
+//   Navbar,
+//   Tech,
+//   StarsCanvasÍ,
+// } from "./components";
+
+const App = () => {
+  return (
+  //   <BrowserRouter>
+  //   <div className="relative z-0 bg-primary">
+  //     <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+  //       <Navbar />
+  //       <Hero />ç
+  //     </div>
+  //   </div>
+  // </BrowserRouter>
+<div>wwwasdasdasasdasdasd</div>
+  );
+};
+export const u = 12;
+export default App;
+
